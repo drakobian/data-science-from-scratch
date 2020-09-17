7041223f8d282db2e03c28017c70bb3416237bfd
drakobian <drake.bennion@gmail.com>
2020-09-16 23:09:46 -0600

    feat: add array of number of friends by id

--- chapter_01.js
+++ chapter_01.js
@@ -3,6 +3,7 @@ const { users, friendship_pairs } = require('./data.json');
 // https://dev.to/devtronic/javascript-map-an-array-of-objects-to-a-dictionary-3f42
 const friendships = Object.assign({}, ...users.map((user) => ({[user.id]: []})));
 
+// make a map from user Id to friend Ids
 for (const friendship of friendship_pairs) {
     friendships[friendship[0]].push(friendship[1]);
     friendships[friendship[1]].push(friendship[0]);
@@ -15,9 +16,18 @@ function number_of_friends(user) {
     return friend_ids.length;
 }
 
+// count up the total number of friendships
 const total_connections = users.reduce(function(total, currentUser){
     return total + number_of_friends(currentUser);
 }, 0);
 
 const num_users = users.length;
+// and find the average number of connections each user has
 const avg_connections = total_connections / num_users;
+
+// make an array of arrays where the first item is
+// the userId and the second item is how many friends they have
+num_friends_by_id = users.map(user => [user.id, number_of_friends(user)])
+
+// sort this array by number of friends, largest to smallest
+num_friends_by_id.sort((list1, list2) => list2[1] - list1[1])
