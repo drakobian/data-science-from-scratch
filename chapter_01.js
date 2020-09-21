58f607f8086d98a0fc091a16d9e445648dee90d5
drakobian <drake.bennion@gmail.com>
2020-09-21 17:30:12 -0600

    refactor: change naming to camelcase

--- chapter_01.js
+++ chapter_01.js
@@ -1,4 +1,4 @@
-const { users, friendship_pairs } = require('./data.json');
+const { users, friendshipPairs } = require('./data.json');
 
 // https://dev.to/devtronic/javascript-map-an-array-of-objects-to-a-dictionary-3f42
 const mapArrayToDict = function(objs, key) {
@@ -13,34 +13,35 @@ const mapArrayToDict = function(objs, key) {
 const friendships = mapArrayToDict(users, 'id');
 
 // make a map from user Id to friend Ids
-for (const friendship of friendship_pairs) {
+for (const friendship of friendshipPairs) {
     friendships[friendship[0]].push(friendship[1]);
     friendships[friendship[1]].push(friendship[0]);
 }
 
 /** How many friends does user have? */
-function number_of_friends(user) {
-    user_id = user.id
-    friend_ids = friendships[user_id];
-    return friend_ids.length;
+const numberOfFriends = function(user, friendshipsMap) {
+    userId = user.id
+    friendIds = friendshipsMap[userId];
+    return friendIds.length;
 }
 
 // count up the total number of friendships
-const total_connections = users.reduce(function(total, currentUser){
-    return total + number_of_friends(currentUser);
+const totalConnections = users.reduce(function(total, currentUser){
+    return total + numberOfFriends(currentUser, friendships);
 }, 0);
 
 const num_users = users.length;
 // and find the average number of connections each user has
-const avg_connections = total_connections / num_users;
+const avgConnections = totalConnections / num_users;
 
 // make an array of arrays where the first item is
 // the userId and the second item is how many friends they have
-num_friends_by_id = users.map(user => [user.id, number_of_friends(user)])
+numberOfFriendsById = users.map(user => [user.id, numberOfFriends(user, friendships)])
 
 // sort this array by number of friends, largest to smallest
-num_friends_by_id.sort((list1, list2) => list2[1] - list1[1])
+numberOfFriendsById.sort((list1, list2) => list2[1] - list1[1])
 
 module.exports = {
-    mapArrayToDict
+    mapArrayToDict,
+    numberOfFriends,
 }
